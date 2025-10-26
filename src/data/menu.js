f290e29a3ee9b4d1fcc1460cb1d34ddd884fd71d
Priyanka Kumari <kumaripriyanka94528@gmail.com>
2025-10-26 19:02:07 +0530

    Add files via upload

---
+++ src/data/menu.js
@@ -0,0 +1,19 @@
+const menu = [
+  {
+    id: 1,
+    name: "Espresso",
+    description: "Rich Italian espresso shot.",
+    price: 120,
+    image: "https://media.istockphoto.com/id/1859646927/photo/closeup-image-of-a-man-and-a-woman-clinking-white-coffee-mugs-in-cafe.webp?b=1&s=612x612&w=0&k=20&c=65GrpOlHHFI6xclmLItHEvIxTP3TlLIKrbcWDaEPjng="
+  },
+  {
+    id: 2,
+    name: "Margherita",
+    description: "Classic pizza with fresh basil.",
+    price: 399,
+    image: "https://i.pinimg.com/736x/5a/21/84/5a2184cf3268f5321481f506ffaceaf7.jpg"
+  },
+  
+];
+
+export default menu;
